34f6d8dc52fd4cc4e670ad929d44d844df8efb17
Nitesh Kumar <142231406+Nitesh-kr30@users.noreply.github.com>
2024-06-24 18:50:26 +0000

    datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+"use strict" //this treats all JS code as newer version
+
+console.log(3+3)
+console.log("Nitesh")
+
+let name = "Nitesh"
+let age = 19
+let isLoggedIn = false
+
+console.log(typeof undefined)
+console.log(typeof null);
