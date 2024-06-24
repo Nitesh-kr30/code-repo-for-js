c62d5a9efe909fd8fb82673322217a145562c65a
Nitesh Kumar <142231406+Nitesh-kr30@users.noreply.github.com>
2024-06-24 19:00:05 +0000

    conversion mid

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,15 @@
+// let score = "33"
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+//if score is of the format 33abc then also it will be converted using the Number function but printing will return NaN
+
+// console.log(valueInNumber);
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+let isLoggedIn = "it"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
