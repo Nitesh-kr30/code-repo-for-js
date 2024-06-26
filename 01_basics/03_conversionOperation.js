4a5f4a60fe16ef16751f77d19e3a1f085cc232c2
Nitesh Kumar <142231406+Nitesh-kr30@users.noreply.github.com>
2024-06-26 11:46:11 +0000

    dataype summary

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -10,6 +10,41 @@
 // "33" => 33
 // "33abc" => NaN
 // true => 1; false => 0
-let isLoggedIn = "it"
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let isLoggedIn = "it"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//***************operation *************/
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+
+// let str1 = "hello"
+// let str2 = "hitesh"
+// let str3 = str1 + str2
+// console.log(str3);
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log(1 + 2 + "2");
+// console.log("1" + 2 + 2);
+
+// console.log(+true);
+// let num1, num2, num3
+// num1 = num2 = num3 = 2+2
+// let gameCounter =100
+// gameCounter++;
+// console.log(gameCounter);
+
+// console.log(null >= 0); 
+//gives true rest gives false as comparison and equality check behave differently in javascript
+// console.log(null > 0);
+// console.log(null == 0);
+
+// console.log(undefined == 0)
+
+// === known as strict check, checks data and datatype too
+
+console.log("2" === 2);
---
+++ 01_basics/dataTypes-Summary.js
@@ -0,0 +1,33 @@
+// Primitive Datatype
+// 7 categories : string, Number, Boolean, null, undefined, Symbol(used to make values uniques), BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail; 
+
+const id= Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+//Reference (Non Primitive)
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "nagraj", "doga"]
+let myObj = {
+    name : "nitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof myObj);
+console.log(typeof myFunction);
+console.log(typeof anotherId);
+
+
